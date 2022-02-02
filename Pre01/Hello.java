a9c13f7213bc442cf97e5ac7f442de2a64cd84ab
vpolon <98716832+vpolon@users.noreply.github.com>
2022-02-02 01:14:51 +0000

    Pre01:I changed the saying and added a positive remark.

---
+++ Pre01/Hello.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+  public static void main(String[] args){
+    System.out.println("Hello world!");
+    System.out.println("Hope you have a great day!");
+   }
+}
